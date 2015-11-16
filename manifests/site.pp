92731ba603d300f9a0cdc24c818a24b8d41676c5
tpurkerson <purkerson@yahoo.com>
2015-11-16 15:17:13 -0800

    Managing /etc/motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -40,4 +40,17 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+  
+  file { '/etc/motd':                                                                                                                                                                                
+  ensure  => file,                                                                                                                                                                                 
+  owner   => 'root',                                                                                                                                                                               
+  group   => 'root',                                                                                                                                                                               
+  mode    => '0644',                                                                                                                                                                               
+  content => "This has been changed in github!\n",                                                                                                                                                              
+}                                                                                                                                                                                                  
+                                                                                                                                                                                                   
+package { 'cowsay':                                                                                                                                                                                
+  ensure   => present,                                                                                                                                                                             
+  provider => gem,                                                                                                                                                                                 
+}                        
 }
