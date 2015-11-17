9e69ad208ada6a24accf0adb86df6a6bad88a87f
tpurkerson <purkerson@yahoo.com>
2015-11-17 15:37:26 -0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,30 +3,40 @@ class nginx {
     ensure => present,
   }
 
-  file { '/var/www': ensure => directory, owner => 'root', group => 'root', mode => '0755',
-}
+  file { '/var/www':
+    ensure => directory, 
+    owner => 'root', 
+    group => 'root', 
+    mode => '0755',
+    }
 
-file { '/var/www/index.html': ensure => file,
-owner => 'root',
-group => 'root',
-           mode   => '0644',
-source => 'puppet:///modules/nginx/index.html', }
+  file { '/var/www/index.html': 
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/nginx/index.html',
+    }
 
-file { '/etc/nginx/nginx.conf':
-ensure => file,
-source => 'puppet:///modules/nginx/nginx.conf',
-}
+  file { '/etc/nginx/nginx.conf':
+    ensure => file,
+    source => 'puppet:///modules/nginx/nginx.conf',
+    }
 
-file { '/etc/nginx/conf.d': ensure => directory, owner => 'root',
-group => 'root',
-           mode   => '0755',
-         }
+  file { '/etc/nginx/conf.d': 
+    ensure => directory, 
+    owner => 'root',
+    group => 'root',
+    mode   => '0755',
+    }
          
-         file { '/etc/nginx/conf.d/default.conf':
-ensure => file,
-source => 'puppet:///modules/nginx/default.conf',
-}
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    source => 'puppet:///modules/nginx/default.conf',
+    }
 
-service { 'nginx': ensure => running, enable => true,
-}
+  service { 'nginx': 
+    ensure => running, 
+    enable => true,
+    }
 }
