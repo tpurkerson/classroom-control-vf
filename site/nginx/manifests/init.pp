f2be59556325f1de0d0ceb9c00cca90e85f3230b
tpurkerson <purkerson@yahoo.com>
2015-11-19 10:12:22 -0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,29 +1,12 @@
 class nginx (
-     $root = undef,
-     ) {
-           case $::osfamily {
-           'redhat','debian' : { $package = 'nginx'
-                      $owner = 'root'
-                      $group = 'root'
-                      $docroot = '/var/www' $confdir = '/etc/nginx' $logdir = '/var/log/nginx'
-           }
-           'windows' : {
-                      $package = 'nginx-service'
-                      $owner = 'Administrator'
-                      $group = 'Administrators'
-                      $docroot = 'C:/ProgramData/nginx/html' 
-                      $confdir = 'C:/ProgramData/nginx' 
-                      $logdir = 'C:/ProgramData/nginx/logs'
-           }
-           default : {
-                      fail("Module ${module_name} is not supported on ${::osfamily}") 
-           }
-}
-
-# user the service will run as. Used in the nginx.conf.erb template
-$user = $::osfamily ? { 
-           'redhat' => 'nginx', 'debian' => 'www-data', 'windows' => 'nobody',
-           }
+     $package = $nginx::params::package,
+     $owner = $nginx::params::owner,
+     $group = $nginx::params::group,
+     $docroot = $nginx::params::docroot,
+     $confdir = $nginx::params::confdir,
+     $logdir = $nginx::params::logdir,
+     $user = $nginx::params::user,
+) inherits nginx::params {
 
            File {
            owner => $owner,
