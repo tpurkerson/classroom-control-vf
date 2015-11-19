3e8c6e860231199eb96a6046d47ea04e378acf4f
tpurkerson <purkerson@yahoo.com>
2015-11-19 08:16:47 -0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,38 +1,52 @@
 class nginx {
 case $::osfamily {
-'redhat','debian' : { $package = 'nginx' $owner = 'root' $group = 'root' $docroot = '/var/www' $confdir = '/etc/nginx'
-           }
+           'redhat','debian' : { 
+                      $package = 'nginx' $owner = 'root' $group = 'root' $docroot = '/var/www' $confdir = '/etc/nginx'
+                      }
            'windows' : {
-$package = 'nginx-service'
-$owner = 'Administrator'
-$group = 'Administrators'
-$docroot = 'C:/ProgramData/nginx/html' $confdir = 'C:/ProgramData/nginx'
-}
-default : {
-fail("Module ${module_name} is not supported on ${::osfamily}") }
-}
-         File {
+                      $package = 'nginx-service'
+                      $owner = 'Administrator'
+                      $group = 'Administrators'
+                      $docroot = 'C:/ProgramData/nginx/html' $confdir = 'C:/ProgramData/nginx'
+                      }
+           default : {
+                      fail("Module ${module_name} is not supported on ${::osfamily}")
+                      }
+           }
+File {
            owner => $owner,
            group => $group,
            mode  => '0664',
 }
+
 package { $package:
-ensure => present, }
-file { [ $docroot, "${confdir}/conf.d" ]: ensure => directory,
+           ensure => present, 
 }
+
+file { [ $docroot, "${confdir}/conf.d" ]: 
+           ensure => directory,
+}
+
 file { "${docroot}/index.html":
-ensure => file,
-source => 'puppet:///modules/nginx/index.html',
+           ensure => file,
+#           source => 'puppet:///modules/nginx/index.html',
+           source => template('nginx/index.html.erb'),
 }
+
 file { "${confdir}/nginx.conf":
-ensure => file,
-source => "puppet:///modules/nginx/${::osfamily}.conf", notify => Service['nginx'],
+           ensure => file,
+           source => "puppet:///modules/nginx/${::osfamily}.conf", 
+           notify => Service['nginx'],
 }
+
 file { "${confdir}/conf.d/default.conf":
-ensure => file,
-source => "puppet:///modules/nginx/default-${::kernel}.conf", notify => Service['nginx'],
+           ensure => file,
+           source => "puppet:///modules/nginx/default-${::kernel}.conf", 
+           notify => Service['nginx'],
 }
+
 service { 'nginx':
-ensure => running,
-enable => true, }
+           ensure => running,
+           enable => true, 
+}
 }
