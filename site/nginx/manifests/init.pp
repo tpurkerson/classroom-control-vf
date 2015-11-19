3e3a58bb7113be6bb9e0d0dc9ca6f42ed23f5a03
tpurkerson <purkerson@yahoo.com>
2015-11-19 08:57:07 -0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,52 +1,57 @@
 class nginx {
-case $::osfamily {
-           'redhat','debian' : { 
-                      $package = 'nginx' $owner = 'root' $group = 'root' $docroot = '/var/www' $confdir = '/etc/nginx'
-                      }
+           case $::osfamily {
+           'redhat','debian' : { $package = 'nginx'
+                      $owner = 'root'
+                      $group = 'root'
+                      $docroot = '/var/www' $confdir = '/etc/nginx' $logdir = '/var/log/nginx'
+           }
            'windows' : {
                       $package = 'nginx-service'
                       $owner = 'Administrator'
                       $group = 'Administrators'
-                      $docroot = 'C:/ProgramData/nginx/html' $confdir = 'C:/ProgramData/nginx'
-                      }
-           default : {
-                      fail("Module ${module_name} is not supported on ${::osfamily}")
-                      }
+                      $docroot = 'C:/ProgramData/nginx/html' 
+                      $confdir = 'C:/ProgramData/nginx' 
+                      $logdir = 'C:/ProgramData/nginx/logs'
            }
-File {
+           default : {
+                      fail("Module ${module_name} is not supported on ${::osfamily}") 
+           }
+}
+
+# user the service will run as. Used in the nginx.conf.erb template
+$user = $::osfamily ? { 
+           'redhat' => 'nginx', 'debian' => 'www-data', 'windows' => 'nobody',
+           }
+
+           File {
            owner => $owner,
            group => $group,
            mode  => '0664',
+           }
+
+package { $package: 
+ensure => present,
 }
 
-package { $package:
-           ensure => present, 
-}
-
-file { [ $docroot, "${confdir}/conf.d" ]: 
+file { [ $docroot, "${docroot}/vhosts", "${confdir}/conf.d" ]:
            ensure => directory,
 }
 
-file { "${docroot}/index.html":
-           ensure => file,
-#           source => 'puppet:///modules/nginx/index.html',
-           content => template('nginx/index.html.erb'),
-}
+# manage the default docroot, index, and conf--replaces several resources 
+nginx::vhost { 'default':
+           docroot    => $docroot,
+           servername => $::fqdn, 
+           }
 
 file { "${confdir}/nginx.conf":
            ensure => file,
-           source => "puppet:///modules/nginx/${::osfamily}.conf", 
+           content => template('nginx/nginx.conf.erb'), 
            notify => Service['nginx'],
 }
 
-file { "${confdir}/conf.d/default.conf":
-           ensure => file,
-           source => "puppet:///modules/nginx/default-${::kernel}.conf", 
-           notify => Service['nginx'],
-}
+service { 'nginx': 
+           ensure => running, 
+           enable => true,
+           } 
 
-service { 'nginx':
-           ensure => running,
-           enable => true, 
-}
 }
