8830f5c2132a54914de77394574c8b928d22b1a5
tpurkerson <purkerson@yahoo.com>
2015-11-19 07:34:43 -0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,52 +1,38 @@
-class nginx { 
-  $wwwdir = '/var/www'
-  
-  File {
-  group=> 'root',
-  }
-  
-  package { 'nginx':
-    ensure => present,
-  }
-
-  file { "${wwwdir}":
-    ensure => directory, 
-    owner => 'root', 
-#    group => 'root', 
-    mode => '0755',
-    }
-
-  file { "${wwwdir}/index.html": 
-    ensure => file,
-    owner => 'root',
-#    group => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/nginx/index.html',
-    }
-
-  file { '/etc/nginx/nginx.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-    notify => Service['nginx'],
-    }
-
-  file { '/etc/nginx/conf.d': 
-    ensure => directory, 
-    owner => 'root',
-#    group => 'root',
-    mode   => '0755',
-    }
-         
-  file { '/etc/nginx/conf.d/default.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
-    notify => Service['nginx'],
-    }
-
-  service { 'nginx': 
-    ensure => running, 
-    enable => true,
-    }
+class nginx {
+case $::osfamily {
+'redhat','debian' : { $package = 'nginx' $owner = 'root' $group = 'root' $docroot = '/var/www' $confdir = '/etc/nginx'
+           }
+           'windows' : {
+$package = 'nginx-service'
+$owner = 'Administrator'
+$group = 'Administrators'
+$docroot = 'C:/ProgramData/nginx/html' $confdir = 'C:/ProgramData/nginx'
+}
+default : {
+fail("Module ${module_name} is not supported on ${::osfamily}") }
+}
+         File {
+           owner => $owner,
+           group => $group,
+           mode  => '0664',
+}
+package { $package:
+ensure => present, }
+file { [ $docroot, "${confdir}/conf.d" ]: ensure => directory,
+}
+file { "${docroot}/index.html":
+ensure => file,
+source => 'puppet:///modules/nginx/index.html',
+}
+file { "${confdir}/nginx.conf":
+ensure => file,
+source => "puppet:///modules/nginx/${::osfamily}.conf", notify => Service['nginx'],
+}
+file { "${confdir}/conf.d/default.conf":
+ensure => file,
+source => "puppet:///modules/nginx/default-${::kernel}.conf", notify => Service['nginx'],
+}
+service { 'nginx':
+ensure => running,
+enable => true, }
 }
